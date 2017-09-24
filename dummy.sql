c85977ff36b022a549368c72594d368f069b4dac
jason chan <jasonchan@jasons-MacBook-Pro.local>
2017-09-24 12:25:36 -0700

    updated dummy.sql

--- dummy.sql
+++ dummy.sql
@@ -1,3 +1,7 @@
+--create database
+CREATE DATABASE restdb;
+
+--use database and insert GET paramenters
 USE restdb;
 INSERT INTO Users (username, email, createdAt, updatedAt)
 VALUES ("John Doe", "haha@yahoo.com","2008-11-11","2008-11-11");
